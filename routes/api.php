7196e8ec163c812577b264f5476859890af7d772
jona872 <jona_872@hotmail.com>
2021-09-07 18:17:09 -0300

    .env config

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+Route::group(['middleware' => 'api'], function () {
+    Route::get('api', 'APIController@postList');
+    //other routes 
+});
+
 
 Route::get('/post', 'APIController@postList');
 Route::post('/createPost', 'APIController@createPost');
