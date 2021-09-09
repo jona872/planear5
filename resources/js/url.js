3c1e4d70108ff1d007e59e55997e26177bbf9d5d
jona872 <jona_872@hotmail.com>
2021-09-09 17:11:25 -0300

    url change

--- resources/js/url.js
+++ resources/js/url.js
@@ -1,3 +1,5 @@
 // url = 'http://127.0.0.1:8000/api'; //artisan serve
 // url = 'http://localhost/laravel-reactjs-crud/public/api'; //lampp
 url = 'http://planear5.herokuapp.com/api'; //heroku 
+
+//testing
