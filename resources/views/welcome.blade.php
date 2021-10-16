993ae94f2763099eb5f1ec0a4e3e90b879c0d225
jona872 <jona_872@hotmail.com>
2021-10-16 15:28:09 -0300

    sorting files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,8 +68,6 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                    <br/>
-                    <br/>
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
