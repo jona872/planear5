f0475f15d23d6221221791beeb08bec4f00ce320
jona872 <jona_872@hotmail.com>
2022-02-15 16:15:38 -0300

    add new map geocoder

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -5,46 +5,60 @@
     <meta charset="utf-8">
     <title>Locate the user</title>
     <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no">
-    <link href="https://api.mapbox.com/mapbox-gl-js/v2.5.0/mapbox-gl.css" rel="stylesheet">
-    <script src="https://api.mapbox.com/mapbox-gl-js/v2.5.0/mapbox-gl.js"></script>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-        }
+    <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.min.js"></script>
+    <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.7.2/mapbox-gl-geocoder.css" type="text/css">
 
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.6.0/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v2.6.0/mapbox-gl.css' rel='stylesheet' />
+    <style>
         #map {
             position: absolute;
-            top: 0;
-            bottom: 0;
-            width: 100%;
+
         }
     </style>
 </head>
 
 <body>
-    <div id="map"></div>
+    <div>
+        <div id="map" style="width: 50%; height: 50%; margin-top: 8%;"></div>
+        <div><label name="lat" id="lat">-</label> </div>
+        <div><label name="lng" id="lng">-</label> </div>
+    </div>
     <script>
         mapboxgl.accessToken = 'pk.eyJ1Ijoiam9uYXRhbmtpbiIsImEiOiJja3VkNGc5ZXcxNm5yMnFxNnl4aW1vbnFvIn0.xG6ZHnZc21DSsy5MEHZmFQ';
         const map = new mapboxgl.Map({
             container: 'map', // container ID
             style: 'mapbox://styles/mapbox/streets-v11', // style URL
-            center: [-96, 37.8], // starting position
+            center: [-61.51051676003347, -30.727283408601465], // starting position
             zoom: 3 // starting zoom
         });
 
-        // Add geolocate control to the map.
-        map.addControl(
-            new mapboxgl.GeolocateControl({
-                positionOptions: {
-                    enableHighAccuracy: true
-                },
-                // When active the map will receive updates to the device's location as it changes.
-                trackUserLocation: true,
-                // Draw an arrow next to the location dot to indicate which direction the device is heading.
-                showUserHeading: true
+        const marker = new mapboxgl.Marker({
+                draggable: true
             })
-        );
+            .setLngLat([0, 0])
+            .addTo(map);
+
+        function onDragEnd() {
+            const lngLat = marker.getLngLat();
+            document.getElementById('lat').innerHTML = JSON.stringify(lngLat.lat);
+            document.getElementById('lng').innerHTML = JSON.stringify(lngLat.lng);
+        }
+        marker.on('dragend', onDragEnd);
+
+        map.on('click', (e) => {
+            marker.setLngLat([e.lngLat.lng, e.lngLat.lat]).addTo(map);
+            document.getElementById('lat').innerHTML = JSON.stringify(e.lngLat.lat);
+            document.getElementById('lng').innerHTML = JSON.stringify(e.lngLat.lng);
+        });
+
+        const geocoder = new MapboxGeocoder({
+            accessToken: mapboxgl.accessToken,
+            marker: false,
+            mapboxgl: mapboxgl
+        });
+        map.addControl(geocoder);
+
     </script>
 
 </body>
